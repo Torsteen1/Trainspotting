86881dd82187a652668984b94e079d66f6b16d5b
fermd <fermd@o-7155b-04.studat.chalmers.se>
2024-01-29 11:08:35 +0100

    Added logic for TurnCrossings

--- trainspotting/src/Lab1.java
+++ trainspotting/src/Lab1.java
@@ -7,6 +7,8 @@ public class Lab1{
 	private Semaphore topRailAvailable = new Semaphore(1);
 	private Semaphore bottomRailAvailable = new Semaphore(1);
 	private Semaphore topDoubleRailAvailable = new Semaphore(1);
+	private Semaphore topTurnCrossingAvailable = new Semaphore(1);
+	private Semaphore bottomTurnCrossingAvailable = new Semaphore(1);
 
 	public Lab1(int speed1, int speed2) {
 		Thread train1 = new Thread(new Train(1, speed1, this));
@@ -19,7 +21,18 @@ public class Lab1{
 	public Semaphore getPlusJunctionAvailable() {
 		return plusJunctionAvailable;
 	}
-
+	
+	public Semaphore getTopTurnCrossingAvailable() {
+		return topTurnCrossingAvailable;
+	}
+	
+	public Semaphore getTopRailAvailable() {
+		return topRailAvailable;
+	}
+	
+	public Semaphore getBottomRailAvailable() {
+		return bottomTurnCrossingAvailable;
+	}
 }
 
 final class Train implements Runnable{
@@ -27,7 +40,8 @@ final class Train implements Runnable{
 	private int id;
 	private Lab1 lab1;
 	private int speed;
-	private boolean inCrossing = false;
+	private boolean inPlusCrossing = false;
+	private boolean inTurnCrossing = false;
 	
 	public Train(int id, int speed, Lab1 lab1) {
 		try {
@@ -52,28 +66,54 @@ final class Train implements Runnable{
 				if(event.getStatus()== 0x02) continue; //If sensor inactive continue
 				int posX = event.getXpos();
 				int posY = event.getYpos();
-				if(((posX + 2 >= 8 && posX<8) || (posX - 2 <= 8 && posX>8)) && ((posY + 2 >= 7 && posY<7) || (posY - 2 <= 7 && posY>7))) {
-					//The plus-junction
-					if(inCrossing) {
+				if(((posX + 2 >= 8 && posX<8) || (posX - 2 <= 8 && posX>8)) && ((posY + 2 >= 7 && posY<7) || (posY - 2 <= 7 && posY>7))) { //The Plus Junction
+					if(inPlusCrossing) {
 						lab1.getPlusJunctionAvailable().release();
-						inCrossing = false;
+						inPlusCrossing = false;
 					}else {
 						tsi.setSpeed(id, 0);
 						lab1.getPlusJunctionAvailable().acquire();
 						tsi.setSpeed(id, speed);
-						inCrossing = true;
+						inPlusCrossing = true;
 					}
 					
-				}else if(((posX + 2 >= 17 && posX<17) || (posX - 2 <= 17 && posX>17)) && ((posY + 2 >= 7 && posY<7) || (posY - 2 <= 7 && posY>7))){
-					//The top T-junction
-					//If train coming from bottom has left double rail, train coming from left must wait.
-					//Then if train coming from left the turn has to be switched to up, and if from right/down the turn must be switched down
+				}else if((posX - 2 <= 17 && posX>17) && (posY + 2 >= 7 && posY<7)){
+					if(inTurnCrossing) {
+						lab1.getTopTurnCrossingAvailable().release();
+						inTurnCrossing = false;
+					}else {
+						if(posX == 16 && posY == 9) continue;
+						tsi.setSpeed(id, 0);
+						lab1.getTopTurnCrossingAvailable().acquire();
+						tsi.setSpeed(id, speed);
+						inTurnCrossing = true;
+						if(posX == 15 && posY == 7) {
+							tsi.setSwitch(17, 7, 0x01);//Unsure if correct direction for the switch
+						}else if(posX == 15 && posY == 8){
+							tsi.setSwitch(17, 7, 0x02);//Unsure if correct direction for the switch
+						}
+					}
 				}else if(((posX + 2 >= 3 && posX<3) || (posX - 2 <= 3 && posX>3)) && ((posY + 2 >= 11 && posY<11) || (posY - 2 <= 11 && posY>11))){
-					//The bottom T-junction
-					//Coming from right: if train left double rail, wait
+					if(inTurnCrossing) {
+						lab1.getBottomTurnCrossingAvailable().release();
+						inTurnCrossing = false;
+					}else {
+						if(posX == 3 && posY == 9) continue;
+						tsi.setSpeed(id, 0);
+						lab1.getBottomTurnCrossingAvailable().acquire();
+						tsi.setSpeed(id, speed);
+						inTurnCrossing = true;
+						if(posX == 5 && posY == 11) {
+							tsi.setSwitch(17, 7, 0x01);//Unsure if correct direction for the switch
+						}else if(posX == 3 && posY == 13){
+							tsi.setSwitch(17, 7, 0x02);//Unsure if correct direction for the switch
+						}
+					}
 				}else if(false){
 					//Double rail
 					//If both trains have left/come into station, wait till the other train is on the other track 
+					//Take semaphores for the T-junctions
+					//Take semaphores for Top rail and bottom rail
 				}else if(false) {
 					//stations
 				}
@@ -91,4 +131,9 @@ final class Train implements Runnable{
 		
 	}
 	
+}
+
+enum Direction{
+	UP,
+	DOWN
 }
